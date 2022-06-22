c9ddafce671b020e146ae5f45f70b05643663072
ketjj <jorjolianiket@gmail.com>
2022-06-22 15:16:51 +0200

    init - Hello, World!

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>cxxxc</title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, impedit ipsum voluptate iste cumque ad. Delectus fugiat non nemo numquam quas culpa in veniam, veritatis obcaecati inventore voluptas adipisci voluptatem!</p>
+</body>
+</html>
