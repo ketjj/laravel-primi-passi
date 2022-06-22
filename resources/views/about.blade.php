2b77fb1d146d77a61ea18de2c6fd5f0cc90bbea8
ketjj <jorjolianiket@gmail.com>
2022-06-22 16:27:46 +0200

    add navbar to other pages too

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,11 +12,25 @@
     </head>
 <body>
     <header class="d-flex align-items-center justify-content-between">
+      <div class="logo">LOGO</div>
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link active" href="{{ route('home')}}">HOME</a>
+        {{-- <a class="nav-link" href="http://'APP_URL'.'DB_HOST'.:.'DB_PORT'">HOME</a> --}}
+      </li>
+      <li class="nav-item">
+        
+        <a class="nav-link" href="{{ route('about')}}">ABOUT</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('contact')}}">CONTACT US</a>
+      </li>
+    </ul>
        
     </header>  
 
     <main>
-      <p class="container my-5">About</p>
+      <h5 class="container my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi sit qui enim nihil, impedit perferendis quos alias unde. Numquam dolores non voluptate mollitia molestias minima laborum illum doloremque, ipsa quia.</h5>
     </main>
 
 </body>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,7 +12,20 @@
 </head>
 <body>
   <header class="d-flex align-items-center justify-content-between">
-
+    <div class="logo">LOGO</div>
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link active" href="{{ route('home')}}">HOME</a>
+        {{-- <a class="nav-link" href="http://'APP_URL'.'DB_HOST'.:.'DB_PORT'">HOME</a> --}}
+      </li>
+      <li class="nav-item">
+        
+        <a class="nav-link" href="{{ route('about')}}">ABOUT</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('contact')}}">CONTACT US</a>
+      </li>
+    </ul>
   </header>
 
   <main>
